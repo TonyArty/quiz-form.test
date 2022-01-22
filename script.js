d4d1ff1a2c34b0af9f328092f04e55b90965930b
TonyArty <i.openuser@gmail.com>
2022-01-22 20:23:07 +0500

    Events on radio, checkbox. Func Prev Nav, Show Step

---
+++ script.js
@@ -0,0 +1,64 @@
+if(document.body.contains(document.getElementById('quiz'))){
+  const formQuiz = document.getElementById('form_quiz');
+  const step = document.getElementsByClassName('step');
+  const navPrev = document.querySelector('button.left');
+  const navNext = document.querySelector('button.right');
+
+  var currentTab = 0; // Current tab is set to be the first tab (0)
+  showTab(currentTab); // Display the crurrent tab
+
+  function showTab(n) {
+    // This function will display the specified tab of the form...
+    step[n].classList.remove('d-none');
+
+    //fix the Previous/Next buttons:
+    if (n == 0) {
+      navPrev.setAttribute('disabled', 'disabled');
+    } else {
+      navPrev.removeAttribute('disabled', 'disabled');
+    }
+    if (n == 7) {
+      dataTexts.forEach(el => {
+        el.classList.add('d-none');
+      })
+      navPrev.classList.add('d-none');
+      navNext.classList.add('d-none');
+    }
+  }
+
+  function validateForm() {
+    // This function deals with validation of the form inputs (radio, checkbox or range)
+    var x = step;
+    var x, y, i, valid = true;
+    // Get inputs (radio, checkbox or range) of current step
+    y = x[currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+      // Valid step if a input (radio, checkbox or range) is checked...
+      if (y[i].checked || y[i].oninput) return valid;
+    }
+  }
+
+  function nextPrev(n) {
+    if (n == 1 && !validateForm()) return false;
+    // Hide the current tab:
+    step[currentTab].classList.add('d-none');
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+  }
+
+  // Next step by click radio button
+  formQuiz.querySelectorAll('input[type=radio]').forEach(el => {
+    el.addEventListener('click', () => {
+      nextPrev(1);
+    })
+  })
+  // Shake next button by click checkbox button
+  formQuiz.querySelectorAll('input[type=checkbox]').forEach(el => {
+    el.addEventListener('click', () => {
+      navNext.classList.toggle('shake');
+    })
+  })
+}
