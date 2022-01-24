38dd2192511254b86d4724392722768ef99cec58
TonyArty <i.openuser@gmail.com>
2022-01-24 22:50:13 +0500

    Test php mail()

---
+++ send.php
@@ -0,0 +1,13 @@
+<?php
+$sender = 'someone@somedomain.tld';
+$recipient = 'you@yourdomain.tld';
+
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers)) {
+  echo "Message accepted";
+} else {
+  echo "Error: Message not accepted";
+}
